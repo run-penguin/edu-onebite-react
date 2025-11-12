6b34710c9593d16b461d2bda661679bdcd06e125
runpenguin <run-penguin@naver.com>
2025-11-12 17:11:43 +0900

    useEffect 사용하기

---
+++ section07/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import Viewer from "./components/Viewer";
+import Controller from "./components/Controller";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    console.log(`count: ${count} / input: ${input}`);
+  }, [count, input]);
+  // 의존성 배열
+  // dependency array (deps)
+
+  const onClickButton = (value) => {
+    // setCount는 비동기로 실행됨
+    setCount(count + value);
+  };
+
+  return (
+    <div className="App">
+      <h1>Simple Counter</h1>
+
+      <section>
+        <input
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+        />
+      </section>
+
+      <section>
+        <Viewer count={count} />
+      </section>
+
+      <section>
+        <Controller onClickButton={onClickButton} />
+      </section>
+    </div>
+  );
+}
+
+export default App;
