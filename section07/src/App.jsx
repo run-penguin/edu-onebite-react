078dc7423991f271128fab291567eee0d6d503bc
runpenguin <run-penguin@naver.com>
2025-11-12 17:20:35 +0900

    useEffect로 라이프사이클 제어하기

--- section07/src/App.jsx
+++ section07/src/App.jsx
@@ -1,17 +1,33 @@
-import { useState, useEffect } from "react";
 import "./App.css";
 import Viewer from "./components/Viewer";
 import Controller from "./components/Controller";
+import Even from "./components/Even";
+import { useState, useEffect, useRef } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
   const [input, setInput] = useState("");
 
+  const isMount = useRef(false);
+
+  /**
+   * 라이프사이클
+   * 1. 마운트 : 탄생
+   * 2. 업데이트 : 변화, 리렌더링
+   * 3. 언마운트 : 죽음
+   */
+
   useEffect(() => {
-    console.log(`count: ${count} / input: ${input}`);
-  }, [count, input]);
-  // 의존성 배열
-  // dependency array (deps)
+    console.log("mount");
+  }, []);
+
+  useEffect(() => {
+    if (!isMount.current) {
+      isMount.current = true;
+      return;
+    }
+    console.log("update");
+  });
 
   const onClickButton = (value) => {
     // setCount는 비동기로 실행됨
@@ -33,6 +49,7 @@ function App() {
 
       <section>
         <Viewer count={count} />
+        {count % 2 === 0 ? <Even /> : null}
       </section>
 
       <section>
