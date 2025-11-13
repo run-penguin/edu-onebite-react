3eea4a852c61b4061f226ca98800a8d864797746
runpenguin <run-penguin@naver.com>
2025-11-13 17:20:13 +0900

    투두리스트 - 수정 기능

--- section08/src/components/List.jsx
+++ section08/src/components/List.jsx
@@ -2,7 +2,7 @@ import "./List.css";
 import TodoItem from "./TodoItem";
 import { useState } from "react";
 
-const List = ({ todos }) => {
+const List = ({ todos, onUpdate }) => {
   const [search, setSearch] = useState("");
 
   const onChangeSearch = (e) => {
@@ -31,7 +31,7 @@ const List = ({ todos }) => {
       />
       <div className="todos_wrapper">
         {filteredTodos.map((todo) => {
-          return <TodoItem key={todo.id} {...todo} />;
+          return <TodoItem key={todo.id} {...todo} onUpdate={onUpdate} />;
         })}
       </div>
     </div>
--- section08/src/components/TodoItem.jsx
+++ section08/src/components/TodoItem.jsx
@@ -1,9 +1,13 @@
 import "./TodoItem.css";
 
-const TodoItem = ({ id, isDone, content, date }) => {
+const TodoItem = ({ id, isDone, content, date, onUpdate }) => {
+  const onChangeCheckbox = () => {
+    onUpdate(id);
+  };
+
   return (
     <div className="TodoItem">
-      <input type="checkbox" checked={isDone} readOnly />
+      <input type="checkbox" checked={isDone} onChange={onChangeCheckbox} />
       <div className="content">{content}</div>
       <div className="date">{new Date(date).toLocaleDateString()}</div>
       <button>삭제</button>
