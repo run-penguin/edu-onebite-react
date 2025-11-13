d7e203bbbe9ccd0ba1d49eaebdd8fb5d65c85f36
runpenguin <run-penguin@naver.com>
2025-11-13 16:08:24 +0900

    투두리스트 - 기능 구현 준비

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,10 +1,35 @@
 import "./App.css";
 
+import { useState, usestate } from "react";
+
 import Header from "./components/Header";
 import Editor from "./components/Editor";
 import List from "./components/List";
 
+// 임시 데이터
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "노래 연습하기",
+    date: new Date().getTime(),
+  },
+];
+
 function App() {
+  const [todos, setTodos] = useState(mockData);
   return (
     <div className="App">
       <Header />
