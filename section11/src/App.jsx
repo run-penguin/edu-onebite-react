1506c63ddf6d7b541acca173982141988ce665d5
runpenguin <run-penguin@naver.com>
2025-11-14 16:10:26 +0900

    Context 분리하기

--- section11/src/App.jsx
+++ section11/src/App.jsx
@@ -6,6 +6,7 @@ import {
   useReducer,
   useCallback,
   createContext,
+  useMemo,
 } from "react";
 
 import Header from "./components/Header";
@@ -50,7 +51,8 @@ function reducer(state, action) {
 }
 
 // App 컴포넌트가 실행될 때 마다 새로 생성하지 않도록 외부에 선언
-export const TodoContext = createContext();
+export const TodoStateContext = createContext();
+export const TodoDispatchContext = createContext();
 
 function App() {
   const [todos, dispatch] = useReducer(reducer, mockData);
@@ -82,20 +84,20 @@ function App() {
     });
   }, []);
 
+  const memoizedDispatch = useMemo(() => {
+    return { onCreate, onUpdate, onDelete };
+  }, []);
+
   return (
     <div className="App">
       <Header />
-      <TodoContext.Provider
-        value={{
-          todos,
-          onCreate,
-          onUpdate,
-          onDelete,
-        }}
-      >
-        <Editor />
-        <List />
-      </TodoContext.Provider>
+
+      <TodoStateContext.Provider value={todos}>
+        <TodoDispatchContext.Provider value={memoizedDispatch}>
+          <Editor />
+          <List />
+        </TodoDispatchContext.Provider>
+      </TodoStateContext.Provider>
     </div>
   );
 }
