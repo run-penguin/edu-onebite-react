e86769f5fb236038df1799eee80f29a97c0b239b
runpenguin <run-penguin@naver.com>
2025-11-14 16:43:18 +0900

    감정일기장 - 라우팅, 페이지 이동

---
+++ section12/src/App.jsx
@@ -0,0 +1,34 @@
+import "./App.css";
+
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Diary from "./pages/Diary";
+import New from "./pages/New";
+import Notfound from "./pages/Notfound";
+
+function App() {
+  const nav = useNavigate();
+  const onClickButton = () => {
+    nav("/new");
+  };
+  return (
+    <>
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/new"}>New</Link>
+        <Link to={"/diary"}>Diary</Link>
+      </div>
+
+      <button onClick={onClickButton}>New 페이지로 이동</button>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/new" element={<New />}></Route>
+        <Route path="/diary" element={<Diary />}></Route>
+        <Route path="*" element={<Notfound />}></Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
