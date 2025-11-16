9ae4708522e9455f2e9ae4b7f1fcaaf16be00b0d
runpenguin <run-penguin@naver.com>
2025-11-16 17:07:47 +0900

    오타 수정

--- section12/src/components/EmotionItem.jsx
+++ section12/src/components/EmotionItem.jsx
@@ -4,7 +4,9 @@ import { getEmotionImage } from "../util/get-emotion-image";
 const EmotionItem = ({ emotionId, emotionName, isSelected, onClick }) => {
   return (
     <div
-      class={`EmotionItem ${isSelected ? `EmotionItem_on_${emotionId}` : ""}`}
+      className={`EmotionItem ${
+        isSelected ? `EmotionItem_on_${emotionId}` : ""
+      }`}
       onClick={onClick}
     >
       <img src={getEmotionImage(emotionId)} className="emotion_img" />
