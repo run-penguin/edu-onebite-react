f6bc5c23d1d1a760714d6e10986841857fb26b76
runpenguin <run-penguin@naver.com>
2025-11-16 17:03:30 +0900

    New 페이지 기능 구현

--- section12/src/components/EmotionItem.jsx
+++ section12/src/components/EmotionItem.jsx
@@ -1,10 +1,11 @@
 import "./EmotionItem.css";
 import { getEmotionImage } from "../util/get-emotion-image";
 
-const EmotionItem = ({ emotionId, emotionName, isSelected }) => {
+const EmotionItem = ({ emotionId, emotionName, isSelected, onClick }) => {
   return (
     <div
       class={`EmotionItem ${isSelected ? `EmotionItem_on_${emotionId}` : ""}`}
+      onClick={onClick}
     >
       <img src={getEmotionImage(emotionId)} className="emotion_img" />
       <div className="emotion_name">{emotionName}</div>
--- section12/src/pages/New.jsx
+++ section12/src/pages/New.jsx
@@ -2,14 +2,32 @@ import Header from "../components/Header";
 import Button from "../components/Button";
 import Editor from "../components/Editor";
 
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { DiaryDispatchContext } from "../App";
+
 const New = () => {
+  const nav = useNavigate();
+  const { onCreate } = useContext(DiaryDispatchContext);
+
+  const onSubmit = (input) => {
+    onCreate(input.createdDate.getTime(), input.emotionId, input.content);
+    nav("/", { replace: true });
+  };
   return (
     <div>
       <Header
         title={"새 일기 쓰기"}
-        leftChild={<Button text={"< 뒤로가기"} />}
+        leftChild={
+          <Button
+            text={"< 뒤로가기"}
+            onClick={() => {
+              nav(-1);
+            }}
+          />
+        }
       />
-      <Editor />
+      <Editor onSubmit={onSubmit} />
     </div>
   );
 };
